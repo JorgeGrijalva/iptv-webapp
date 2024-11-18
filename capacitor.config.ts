122d69952272944017c644ad7e26ab7732019957
JorgeGrijalva <georgegrijalva75@gmail.com>
2024-11-18 08:47:59 -0600

    favicon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.miempresa.iptvwebapp',
-  appName: 'NombreApp',
+  appId: 'com.zephyrus.iptv',
+  appName: 'FLUTV',
   webDir: 'build'
 };
 
